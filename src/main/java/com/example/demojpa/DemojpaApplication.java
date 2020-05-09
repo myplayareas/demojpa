e4189c7748665fdd791bbe3ffe6506c90f4a6d8d
armandossrecife <armando.sousa@gmail.com>
2020-05-09 15:55:45 -0300

    Criada a pasta docs e estrutura base de pacotes do exemplo

--- src/main/java/com/example/demojpa/DemojpaApplication.java
+++ src/main/java/com/example/demojpa/DemojpaApplication.java
@@ -1,6 +1,5 @@
 package com.example.demojpa;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
